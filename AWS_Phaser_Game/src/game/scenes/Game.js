931e2309c3f6837f3c33588acd643b0dd4b76a05
niclee1803 <niclee1803@outlook.com>
2025-01-08 13:39:26 +0800

    Added dino player

--- AWS_Phaser_Game/src/game/scenes/Game.js
+++ AWS_Phaser_Game/src/game/scenes/Game.js
@@ -1,5 +1,5 @@
 import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { Scene, Input } from 'phaser';
 
 export class Game extends Scene
 {
@@ -14,13 +14,43 @@ export class Game extends Scene
 
         this.add.image(512, 384, 'background').setAlpha(0.5);
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        // this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        //     stroke: '#000000', strokeThickness: 8,
+        //     align: 'center'
+        // }).setOrigin(0.5).setDepth(100);
+
+        this.dino = this.add.sprite(512,384,"dino").setScale(2);
+
+        this.cursors = this.input.keyboard.addKeys({
+            up: Input.Keyboard.KeyCodes.W,
+            down: Input.Keyboard.KeyCodes.S,
+            left: Input.Keyboard.KeyCodes.A,
+            right: Input.Keyboard.KeyCodes.D
+        });
 
         EventBus.emit('current-scene-ready', this);
+    }
+
+    update ()
+    {
+        if (this.cursors.left.isDown)
+        {
+            this.dino.x -= 5;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.dino.x += 5;
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.dino.y -= 5;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.dino.y += 5;
+        }
     }
 
     changeScene ()

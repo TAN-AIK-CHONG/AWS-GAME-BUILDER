2daff2357f23fd5e6bec7948b3f8323a2ec4bbed
Tan Aik Chong <atan160@e.ntu.edu.sg>
2025-01-16 18:26:53 +0800

    level 2

--- DinoRushGame/src/game/scenes/GameL2.js
+++ DinoRushGame/src/game/scenes/GameL2.js
@@ -38,12 +38,29 @@ export class GameL2 extends GameScene
         const decorations = map.createLayer('Decorations', tileset, 0, 0).setScale(3);
         const foreground = map.createLayer('Foreground', tileset, 0, 0).setScale(3);
         const spikes = map.createLayer('Spikes', tileset, 0, 0).setScale(3);
+        const flag = map.createLayer('Flag', tileset, 0, 0).setScale(3);
 
         foreground.setCollisionByProperty({ collides: true });
         spikes.setCollisionByProperty({ collides: true });
+        flag.setCollisionByProperty({ flag: true });
         
         this.physics.add.collider(this.dino, foreground);
-        this.physics.add.collider(this.dino, spikes, this.loseLife, null, this);  
+        this.physics.add.collider(this.dino, flag, this.handleFlag, null, this);
+        this.physics.add.collider(this.dino, spikes, this.loseLife, null, this); 
+        
+        // Access Gems Object Layer
+        const gemsObjectLayer = map.getObjectLayer('Gems').objects;  // Get array of gem objects
+        this.gemGroup = this.physics.add.group();  // Group to hold gem sprites
+    
+        gemsObjectLayer.forEach((gemObj) => {
+            const gem = this.gemGroup.create(gemObj.x * 3, gemObj.y * 3, 'gem');  // Adjust for scale and origin
+            gem.setOrigin(0, 1);  // Object layers use top-left as origin
+            gem.setScale(3);  // Match tile scale
+            gem.body.setAllowGravity(false);  // Prevent gravity if they are floating gems
+        });
+    
+        // Collision with gems
+        this.physics.add.overlap(this.dino, this.gemGroup, this.collectGem, null, this);
         
         //set boundaries
         this.cameras.main.setBounds(0, 0, map.widthInPixels * 3, map.heightInPixels * 3);
